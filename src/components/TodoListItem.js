380e0acba0286f236c144b741b7649eb1d204092
bees3030 <bees_3030@naver.com>
2021-04-06 20:55:19 +0900

    add

---
+++ src/components/TodoListItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { 
+    MdCheckBoxOutlineBlank,
+    MdCheckBox,
+    MdRemoveCircleOutline
+} from 'react-icons/md';
+import './TodoListItem.css';
+
+const TodoListItems = ({todo, onRemove, onToggle}) => {
+    const {id ,text, checked} = todo;
+
+    return(
+        <div className="TodoListItem">
+            <div className="checkbox" onClick={() => onToggle(id)}>
+                {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
+                <div className="text">{text}</div>
+            </div>
+            
+            <div className="remove" onClick={() => onRemove(id)}>
+                <MdRemoveCircleOutline/>
+            </div>
+        </div>
+    );
+}
+
+export default TodoListItems;
